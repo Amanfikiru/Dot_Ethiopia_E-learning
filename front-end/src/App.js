1cad27e0a7ae106904826108a8b4d88a0d047de7
Tigist Solomon <tigistsolomon501@gmail.com>
2022-10-13 23:38:28 +0300

    Update Login and Registration form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Register from './components/register/register';
+import Home from './components/home/home';
+import Login from './components/login/login';
+function App() {  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="Registration" element={<Register/>}/>        
+        <Route path="Login" element={<Login/>}/>
+        
+        </Routes>
+
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
