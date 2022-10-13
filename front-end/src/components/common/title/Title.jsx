540ae299876760fa13826f4fd2f694115c5454df
Amanfikiru <amanuelfikiru49@gmail.com>
2022-10-13 11:55:32 -0700

    about page

--- front-end/src/components/common/title/Title.jsx
+++ front-end/src/components/common/title/Title.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Title = ({ subtitle, title }) => {
   return (
     <div>
-        <div id='heading'>
+        <section id='heading'>
             <h3>{subtitle}</h3>
             <h1>{title}</h1>
-        </div>
+        </section>
     </div>
   )
 }
