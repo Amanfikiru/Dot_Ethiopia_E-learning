bcc10bfc79489bdf6cf3b44af5d1e1b6c9820d10
Amanfikiru <amanuelfikiru49@gmail.com>
2022-10-13 11:50:20 -0700

    about page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Header from './components/common/heading/Header'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import home from "./components/home/home";
+import About from "./components/about/About";
 
 
 
@@ -13,6 +14,7 @@ function App () {
         <Header />
         <Switch>
           <Route path='/' exact component={home}/>
+          <Route path='/about' exact component={About}/>
         </Switch>
       </Router> 
     </>
