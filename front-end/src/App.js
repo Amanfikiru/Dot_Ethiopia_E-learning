86f34c1fe385b9b56bb57c928804ad687dc433b9
Biruktawit <adamubiruktawit1@gmail.com>
2022-11-07 21:17:06 +0300

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,14 +13,58 @@ import { InstructorEdit } from "./adminall/instructoredit/InstructorEdit";
 import { AdminReport } from "./adminall/adminreport/adminreports/AdminReport";
 import { AdminReportList } from "./adminall/adminreport/AdminReportList";
 import {AdminCourseReport} from "./adminall/adminreport/adminreports/AdminCourseReport";
-
+// This is Student dashboard routes 
+import Sidebar from '../src/components/sidebar/Sidebar';
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/StudentDash/home/Home";
+import CourseList from "./pages/StudentDash/courseList/CourseList";
+import Quiz from "./pages/StudentDash/quiz/Quiz";
+import Chat from "./pages/StudentDash/chat/chat";
+import Feedback from "./pages/StudentDash/feedback/Feedback";
+import Message from "./pages/StudentDash/message/Message";
+import Explore from "./pages/StudentDash/explore/Explore";
+import Assignment from './pages/StudentDash/assignment/Assignment';
 function App () {
   return (
     <Router>
       <Topbar />
       <div className="container">
+         <Sidebar />
         <Adminsidebar />
         <Switch>
+           <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/courses">
+            <CourseList />
+          </Route>
+           <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/courses">
+            <CourseList />
+          </Route>
+           <Route path="/assignment">
+            <Assignment/>
+          </Route>
+           <Route path="/quiz">
+            <Quiz/>
+          </Route>
+           <Route path="/chat">
+            <Chat/>
+          </Route>
+           <Route path="/feedback">
+            <Feedback/>
+          </Route>
+           <Route path="/message">
+            <Message/>
+          </Route>
+           <Route path="/explore">
+            <Explore/>
+          </Route>
+          
+           {/* this student dashboard route  */}
+          
           <Route path="/admin/home">
             <Adminhome />
           </Route>
---
+++ front-end/src/pages/StudentDash/courseList/CourseList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Demo from './Demo';
+const CourseList = () => {
+  return (
+    <div> 
+      <Demo />
+    </div>
+  )
+}
+
+export default CourseList
